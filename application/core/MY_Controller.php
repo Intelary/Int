de8f6b2f5464f1039a7cab61bbcb7b5fcba1c55b
Kelnit <kelvinmulyawan2000@gmail.com>
2026-06-28 14:30:53 +0700

    Int

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+// Out Controller : Sign In & Sign Out & Register
+
+class OutController extends CI_Controller {
+  // Init Controller
+  public function __construct() {
+    // Base Controller Init
+    parent::__construct();
+  }
+}
+
+// Main Controller : Selain Auth Controller
+
+class MainController extends CI_Controller {
+  // Init Controller
+  public function __construct() {
+    // Base Controller Init
+    parent::__construct();
+    // If User isn't Log In
+    if (!loggedin()) {
+      // Set Url to Redirect After Login
+      $this->session->set_userdata('redirect_url', current_url());
+      // Redirect to Login Page
+      redirect(base_url());
+    }
+  }
+}
+
+?>
