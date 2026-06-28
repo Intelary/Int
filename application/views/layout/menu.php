659f42a3a5ebb871b8a4223227e48e7cad8b9cec
Kelnit <kelvinmulyawan2000@gmail.com>
2026-06-28 14:42:13 +0700

    Hi

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -1,3 +1,7 @@
+<style>
+  .hider { display : none }
+</style>
+
 <!-- Panel Label -->
 <?php $segment = $this->uri->segment(1); ?>
 <!-- Panel Pilihan -->
@@ -9,17 +13,17 @@
       <div class="nav-text">Home</div>
     </a>
     <!-- Question -->
-    <a href="<?php echo base_url('inti'); ?>" class="nav-link nav-item-float flex-fill <?php echo ($segment == 'inti') ? 'text-primary active' : 'text-secondary'; ?>">
+    <a href="<?php echo base_url('inti'); ?>" class="nav-link nav-item-float flex-fill <?php echo ($segment == 'inti') ? 'text-primary active' : 'text-secondary'; ?> hider">
       <i class="bi bi-patch-question-fill fs-5"></i>
       <div class="nav-text">Q&A</div>
     </a>
     <!-- Blog -->
-    <a href="<?php echo base_url('blog'); ?>" class="nav-link nav-item-float flex-fill <?php echo ($segment == 'blog') ? 'text-primary active' : 'text-secondary'; ?>">
+    <a href="<?php echo base_url('blog'); ?>" class="nav-link nav-item-float flex-fill <?php echo ($segment == 'blog') ? 'text-primary active' : 'text-secondary'; ?> hider">
       <i class="bi bi-book-half fs-5"></i>
       <div class="nav-text">Blog</div>
     </a>
     <!-- WhatsApp -->
-    <a href="#" target="_blank" class="nav-link nav-item-float flex-fill text-secondary">
+    <a href="#" target="_blank" class="nav-link nav-item-float flex-fill text-secondary hider">
       <i class="bi bi-whatsapp fs-5"></i>
       <div class="nav-text">WhatsApp</div>
     </a>
@@ -29,12 +33,12 @@
       <div class="nav-text">Profile</div>
     </a>
     <!-- App / Pengaturan -->
-    <a href="<?php echo base_url('app'); ?>" class="nav-link nav-item-float flex-fill <?php echo ($segment == 'app') ? 'text-primary active' : 'text-secondary'; ?>">
+    <a href="<?php echo base_url('app'); ?>" class="nav-link nav-item-float flex-fill <?php echo ($segment == 'app') ? 'text-primary active' : 'text-secondary'; ?> hider">
       <i class="bi bi-gear-fill fs-5"></i>
       <div class="nav-text">App</div>
     </a>
     <!-- Pasien -->
-    <a href="<?php echo base_url('pasien'); ?>" class="nav-link nav-item-float flex-fill <?php echo ($segment == 'pasien') ? 'text-primary active' : 'text-secondary'; ?>">
+    <a href="<?php echo base_url('pasien'); ?>" class="nav-link nav-item-float flex-fill <?php echo ($segment == 'pasien') ? 'text-primary active' : 'text-secondary'; ?> hider">
       <i class="bi bi-person-lines-fill fs-5"></i>
       <div class="nav-text">Pasien</div>
     </a>
